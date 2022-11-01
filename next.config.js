01d19b664c9a6149f0cdc9e0c7f727de2186a243
filipvlkous <Thiefcz@email.cz>
2022-11-02 01:54:27 +0200

    eslint ignore

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   swcMinify: true,
   presets: ["@next/babel"],
   dangerouslyAllowSVG: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
