3c99ad18c88a9c7ccad5318a4efcae062808a4e3
filipvlkous <Thiefcz@email.cz>
2023-02-12 12:20:24 +0100

    next.config update

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   swcMinify: true,
-  presets: ["@next/babel"],
-  dangerouslyAllowSVG: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  compress: true,
-  productionBrowserSourceMaps: true,
 };
 
 module.exports = nextConfig;
